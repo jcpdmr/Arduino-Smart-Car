ccdd34a163d44b7abe7295a881d7509faefe44a2
Wardl3ss <91423633+Wardl3ss@users.noreply.github.com>
2021-09-27 16:00:50 +0200

    Add files via upload

--- sensor_helper_board2_v_0/sensor_helper_board2_v_0_ProvaPullRequest.ino
+++ sensor_helper_board2_v_0/sensor_helper_board2_v_0_ProvaPullRequest.ino
@@ -3,13 +3,6 @@ const int alarmPin = 12;
 const int num_samples = 20;
 int last_sum = 0;
 
-void setup() {
-  pinMode(sensorPin, INPUT);
-  pinMode(alarmPin, OUTPUT);
-  //Serial.begin(9600);
-  delay(5000); // this delay is to set up the central MCU and do not let it interpretes some starting signal from this board
-}
-
 void loop() {
   int sample_sum = sum_read();
   //Serial.println("The result of the first sample: " + String(first_sum));
