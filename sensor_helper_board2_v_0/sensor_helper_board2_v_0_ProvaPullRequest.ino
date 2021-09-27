55dbb4219eab707d4123432706177cbfbf0f3620
Wardl3ss <91423633+Wardl3ss@users.noreply.github.com>
2021-09-27 16:02:53 +0200

    Add files via upload

--- sensor_helper_board2_v_0/sensor_helper_board2_v_0_ProvaPullRequest.ino
+++ sensor_helper_board2_v_0/sensor_helper_board2_v_0_ProvaPullRequest.ino
@@ -17,13 +17,5 @@ void loop() {
  delay(2000); //the meaning of this big delay is to prevent the activation of the alarm by accidental stop of the wheel during the change of direction
 }
 
-int sum_read(){
-  int sum = 0;
-  for(int i = 0; i < num_samples; i++){
-    sum = sum + digitalRead(sensorPin);
-    //Serial.println("Attempt: " + String(i) + " | Value: " + String(digitalRead(sensorPin)));
-    delay(30);
-    }
-  return sum; 
-  }
+
 // PROVA PROVA 123 PROVA PROVA 123 PER APRIRE UNA PULL REQUEST
