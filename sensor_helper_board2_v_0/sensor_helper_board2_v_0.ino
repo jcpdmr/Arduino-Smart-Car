2569521647d0f9ea56d85d0bd45f1de2a0ada804
Jacopo Damerini <76707805+Bleffjack@users.noreply.github.com>
2021-09-29 22:11:39 +0200

    Add files via upload

--- sensor_helper_board2_v_0/sensor_helper_board2_v_0.ino
+++ sensor_helper_board2_v_0/sensor_helper_board2_v_0.ino
@@ -1,18 +1,20 @@
 const int sensorPin = 11;
 const int alarmPin = 12;
 const int num_samples = 20;
-int last_sum = 0;
+
 
 void setup() {
   pinMode(sensorPin, INPUT);
   pinMode(alarmPin, OUTPUT);
-  //Serial.begin(9600);
+  Serial.begin(9600);
+  Serial.println("Starting.... [If the result is " + String(num_samples) + " or 0 the car is stucked, else it's moving]");
+  
   delay(5000); // this delay is to set up the central MCU and do not let it interpretes some starting signal from this board
 }
 
 void loop() {
   int sample_sum = sum_read();
-  //Serial.println("The result of the first sample: " + String(first_sum));
+  Serial.println("The result of the sample: " + String(sample_sum));
  
   if(sample_sum == 0 || sample_sum == num_samples){
       digitalWrite(alarmPin, HIGH);
